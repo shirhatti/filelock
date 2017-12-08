3d504f0c7c4068c0c6e941a50bc598599295b9ff
Sourabh Shirhatti <soshir@microsoft.com>
2017-12-08 11:52:30 -0800

    Change to trigger redployment

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace filelock
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World 2017!");
             });
         }
     }
